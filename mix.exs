ecf07b7924efaae637e1839525cbdb408c3e2fab
Stanislav Versilov <stas.versilov@gmail.com>
2019-09-19 16:27:54 +0400

    Bump version.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Base62Id.Mixfile do
   def project do
     [
       app: :base62id,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.6",
       description: description(),
       build_embedded: Mix.env() == :prod,
