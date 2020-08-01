c36cbc8f44d43011a7ea4ab6082d60868688cc5f
Stas Versilov <stas.versilov@gmail.com>
2020-08-01 14:56:07 +0400

    Include letter z in allowed characters.

--- src/base62.erl
+++ src/base62.erl
@@ -32,7 +32,7 @@ encode(I, Acc) ->
     encode(I1, [digit_to_char(I rem 62) | Acc]).
 
 %% Skip non-valid characters, like spaces
-decode([C | T], N, Acc) when (C < $0) or (C >= $0 + 74)  ->
+decode([C | T], N, Acc) when (C < $0) or (C > $0 + 74)  ->
     decode(T, N, Acc);
 decode([C | T], N, Acc) ->
     decode(T, N * 62, char_to_digit(C) * N + Acc);
